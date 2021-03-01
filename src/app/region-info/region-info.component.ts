86b81ddc1b3c6cc6bc62316081d60619aa7fe57e
web-dev <dias.07.04@gmail.com>
2021-03-01 15:46:58 +0600

    add carousel

--- src/app/region-info/region-info.component.ts
+++ src/app/region-info/region-info.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'app-region-info',
 	templateUrl: './region-info.component.html',
-	styleUrls: ['./region-info.component.css']
+	styleUrls: ['./region-info.component.css'],
+	providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
+
 })
 export class RegionInfoComponent implements OnInit {
 
-	constructor(public router: Router) { }
+	// images = [700, 800, 807].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+	constructor(config: NgbCarouselConfig) {
+		// 
+		config.interval = 2000;
+		config.keyboard = true;
+		config.pauseOnHover = true;
+	}
 
 	ngOnInit(): void {
 	}
 
-	route() {
-		this.router.navigateByUrl('region-info/' + '#about-us');
-	}
 
 }
