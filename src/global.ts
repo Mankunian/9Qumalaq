ed5232b6a67739550c07bb978213b5181af8e981
web-dev <dias.07.04@gmail.com>
2021-04-06 13:14:23 +0600

    design main fix

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,4 @@
 export const GlobalConfig = Object.freeze({
 	ADMIN_URL: 'http://195.210.47.86/api/admin/login/?next=/api/admin/',
-	API_URL: 'http://195.210.47.86/api'
+	API_URL: 'http://back.9qq.org/api'
 });
