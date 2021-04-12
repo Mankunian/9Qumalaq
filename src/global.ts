d6bfa77662474bc9279539504b705a94dccd307f
web-dev <dias.07.04@gmail.com>
2021-04-12 15:38:41 +0600

    fix

--- src/global.ts
+++ src/global.ts
@@ -1,4 +1,4 @@
 export const GlobalConfig = Object.freeze({
-	ADMIN_URL: 'https://back.aqyl.host/api/admin/login/?next=/api/admin/',
+	ADMIN_URL: 'https://back.9qq.org/api/admin/login/?next=/api/admin/',
 	API_URL: 'https://back.9qq.org/api'
 });
